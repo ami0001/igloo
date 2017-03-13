b3eeee3ff41d8c8b7993eddf088d6936097fdf0d
ami0001 <ami0001@uah.edu>
2017-03-13 12:16:48 -0500

    adding login/registration

--- addtable.php
+++ addtable.php
@@ -9,74 +9,74 @@ if (mysqli_connect_errno()) {
 
 // Create tables
 $accounts="CREATE TABLE tbAccount(
-			username VARCHAR(32),
-			password VARCHAR(32),
+			email VARCHAR(64),
+			pass VARCHAR(32),
 			fname VARCHAR(32),
 			lname VARCHAR(32),
-			email VARCHAR(64),
 			phone CHAR(10),
 			cardNum CHAR(16),
 			securityCode CHAR(3),
 			expireMonth CHAR(2),
 			expireYear CHAR(2),
 			cardName VARCHAR(21),
-			PRIMARY KEY (username),
-			CONSTRAINT UC_Account UNIQUE (email, cardNum)
+			PRIMARY KEY (email),
+			UNIQUE (cardNum)
 			)";
 $orders="CREATE TABLE tbOrder(
 			orderNum BIGINT,
+			orderTime TIMESTAMP(6),
 			orderName VARCHAR(32),
 			orderStyle VARCHAR(8),
-			orderTime TIMESTAMP,
-			username VARCHAR(32),
-			items VARCHAR(max),
-			basePrice VARCHAR(max),
-			taxPrice VARCHAR(max),
-			overallPrice VARCHAR(max),
+			email VARCHAR(64),
+			items VARCHAR(8000),
+			basePrice VARCHAR(8000),
+			taxPrice VARCHAR(8000),
+			overallPrice VARCHAR(8000),
 			PRIMARY KEY (orderNum),
-			FOREIGN KEY (username) REFERENCES tbAccount (username)
+			UNIQUE(orderTime),
+			FOREIGN KEY (email) REFERENCES tbAccount (email)
 			)";
 $receipts="CREATE TABLE tbReceipt(
-			receiptNum BIGINT
-			receiptName VARCHAR(32),
-			orderTime TIMESTAMP,
-			username VARCHAR(32),
-			items VARCHAR(max),
+			orderNum BIGINT,
+			orderTime TIMESTAMP(6),
+			orderName VARCHAR(32),
+			empId char(9),
+			email VARCHAR(64),
+			items VARCHAR(8000),
 			basePrice FLOAT,
 			taxPrice FLOAT,
 			overallPrice FLOAT,
-			tip VARCHAR(max),
-			waiterName VARCHAR(32),
-			waiter CHAR()
+			tip VARCHAR(8000),
 			cardNum CHAR(16),
-			CONSTRAINT PK_Receipt PRIMARY KEY (receiptNum, orderTime),
+			PRIMARY KEY (orderNum, orderTime),
+			FOREIGN KEY (email) REFERENCES tbAccount (email),
+			FOREIGN KEY (cardNum) REFERENCES tbAccount (cardNum),
+			FOREIGN KEY (empId) REFERENCES tbEmployee (empId),
 			FOREIGN KEY (orderNum) REFERENCES tbOrder (orderNum),
-			FOREIGN KEY (username) REFERENCES tbAccount (username),
-			FOREIGN KEY (cardNum) REFERENCES tbAccount (carNum),
-			FOREIGN KEY (waiter) REFERENCES tbEmployee (empId)
+			FOREIGN KEY (orderTime) REFERENCES tbOrder (orderTime)
 			)";
 $items="CREATE TABLE tbItem(
 			itemId CHAR(7),
 			itemName VARCHAR(256),
 			itemPrice FLOAT,
-			itemDesc VARCHAR(max),
-			PRIMARY KEY (itemID)
+			itemDesc VARCHAR(8000),
+			PRIMARY KEY (itemId)
 			)";
 $employees="CREATE TABLE tbEmployee(
-			empId CHAR(9);
-			fname VARCHAR(32),
+			efname VARCHAR(32),
 			minit VARCHAR(1),
-			lname VARCHAR(32),
+			elname VARCHAR(32),
+			empId CHAR(9),
 			ssn CHAR(9),
-			bdate CHAR(9),
+			bdate CHAR(10),
 			address VARCHAR(128),
 			sex CHAR(1),
 			salary FLOAT,
 			empLvl VARCHAR(8),
-			username VARCHAR(32),
+			email VARCHAR(32),
 			PRIMARY KEY (empId),
 			UNIQUE (ssn),
-			FOREIGN KEY (username) REFERENCES tbAccount(username)
+			FOREIGN KEY (email) REFERENCES tbAccount(email)
 			)";
 			
 // Execute query
@@ -87,27 +87,31 @@ else {
   echo "Error creating table: " . mysqli_error($con) . ".<br>";
 } 
 
-if (mysqli_query($con,$accounts)) {
-  echo "tbAccount table created successfully."."<br>";
+if (mysqli_query($con,$orders)) {
+  echo "tbOrder table created successfully."."<br>";
 }
 else {
   echo "Error creating table: " . mysqli_error($con) . ".<br>";
-  
-if (mysqli_query($con,$accounts)) {
-  echo "tbAccount table created successfully."."<br>";
+} 
+ 
+if (mysqli_query($con,$receipts)) {
+  echo "tbReceipt table created successfully."."<br>";
 }
 else {
   echo "Error creating table: " . mysqli_error($con) . ".<br>";
-  
-if (mysqli_query($con,$accounts)) {
-  echo "tbAccount table created successfully."."<br>";
+}  
+
+if (mysqli_query($con,$items)) {
+  echo "tbItem table created successfully."."<br>";
 }
 else {
   echo "Error creating table: " . mysqli_error($con) . ".<br>";
-  
-if (mysqli_query($con,$accounts)) {
-  echo "tbAccount table created successfully."."<br>";
+}
+
+if (mysqli_query($con,$employees)) {
+  echo "tbEmployee table created successfully."."<br>";
 }
 else {
   echo "Error creating table: " . mysqli_error($con) . ".<br>";
-  
+}
+?>
