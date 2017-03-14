f006455cd9f140f7cffe4ad30991f33f1b0209eb
JonJon <jonshields10@gmail.com>
2017-03-14 13:16:30 -0500

    changes that I made yesterday
    Added a picture to the home page.
    I also included two links to the register page and the login page

--- index.php
+++ index.php
@@ -6,6 +6,20 @@
 	<link href="style.css" rel=stylesheet type="text/css" />
 </head>
 <body>
-<h1>Welcome to the Igloo</h1>
+	<h1>Welcome to the Igloo</h1>
+	
+	<!-- This hyperlink is going to take us to the register.php page -->
+	<a href=register.php>Register for an Account</a>
+	
+	<!-- This hyperlink is going to take us to the login.php page -->
+	<a href=login.php>Login</a>
+	
+	<!-- Attempting to include a picture for the home page -->
+	<img src="images/iglooPic3.jpg" alt="header" title="header" style="width: 90%;">
+	
 </body>
+
+
+
+
 </html>
