d7c6e550b940f9b28f4101c1c5e1e5efd0a5d1fc
JonJon <jonshields10@gmail.com>
2017-03-14 19:20:26 -0500

    Update to the home page
    Following updates were mae to the home page
    
    1. Change the color of the header to blue
    2. Included a slide show in the midle that cycles through each picture.
    Picture 1 is an igloo
    Picture 2 is the inside of the igloo
    Picuture 3 is a smoothie
    
    3. Included two new links to account for the Menu page and the Ordering
    Online page
    
    4. Included the copy right in the footer

--- index.php
+++ index.php
@@ -6,16 +6,51 @@
 	<link href="style.css" rel=stylesheet type="text/css" />
 </head>
 <body>
-	<h1>Welcome to the Igloo</h1>
+	<div id="container">
 	
-	<!-- This hyperlink is going to take us to the register.php page -->
-	<a href=register.php>Register for an Account</a>
+		<div id="header">
+			<h1>Welcome to the Igloo</h1>
+		</div> <!-- End of header -->
 	
-	<!-- This hyperlink is going to take us to the login.php page -->
-	<a href=login.php>Login</a>
+		<!-- this is being used to display the login and register on the same horizontal line -->
+		<div id="div_top_hypers">
+			<ul id="ul_top_hypers">			
+				<li> <a href = ""> Menu </li>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<li> <a href = ""> Order Online </li>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<li> <a href=register.php> Register for an Account</a> </li>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<span style="display:inline-block; width: YOURWIDTH;"></span>
+				<li> <a href=login.php>Login</a> </li>
+			</ul>
+		</div>	<!-- end of div_top_hypers -->
+		
+				
+		
+		<div id="mainContent">
+		
+			<!-- Attempting to include a picture for the home page -->
+			<center>
+				<!-- <img src="images/iglooPic3.jpg" alt="header" title="header" style="width: 90%;"> -->
+				<!-- this is going to present the slideshow on the main page -->
+				<!-- details about the slide show are provide in the css style sheet -->
+				<div class="simple-ss" id="simple-ss"></div>
+			</center>
+		
+		</div> <!-- End of mainContent -->
+				
+		<div id="footer">
+			<p>Copyright &copy; 2017 TheIgloo. All rights reserved.</p>
+		</div> <!-- End of footer -->
 	
-	<!-- Attempting to include a picture for the home page -->
-	<img src="images/iglooPic3.jpg" alt="header" title="header" style="width: 90%;">
+	</div> <!-- End of the Container -->
+	
 	
 </body>
 
