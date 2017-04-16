49fbed3fd9818ba7e59cea04ba86691d7db8854f
JonJon <jonshields10@gmail.com>
2017-04-15 20:52:19 -0500

    update

--- custom.php
+++ custom.php
@@ -4,7 +4,8 @@
 	<title>Custom Order Page</title>
 	<link href="style.css" rel=stylesheet type="text/css" />
 </head>
-<body>
+<!-- body style="background-image:url('images/CustomOrderBackground6.jpg'); background-size: 100% 100%; background-repeat: no-repeat;" -->
+<body style="background-color:#6699CC">
 	<?php
 			require "check.php";
 	?>
@@ -12,12 +13,94 @@
 	<p>
 	<div id = "wrap">
 		<div class = "left">
+			<!-- Size descriptions  -->
 			<p align = "center">
-				Select the Size
+				Size Descriptions
 			</p>
+			<textarea cols="50" rows="8" readonly style = "resize:none">Here are the Igloo we offer four different cup sizes. 
+			
+SnowFlake: This size is for those who would like a small sample.
+
+Icicle: This size is for those who would prefer a small cup.
+
+Iceberg: This size is for those who are up for the challenge.
+
+Glacier: This size is only for champions.
+				 
+			</textarea>
+			
+			<!-- Flavor descriptions  -->
+			<div id="itemDescriptionSpace"></div>
+			<p align = "center">
+				Flavor Descriptions
+			</p>
+			<textarea cols="50" rows="19" readonly style = "resize:none">Here at the Igloo we offer a variety of different flavors.
+			
+There are currently 12 flavors to choose from
+
+Strawberry: Our strawberries are picked from the freshest gardens
+
+Blueberry: Our blueberries are flown in from Paris
+
+Pineapple: Our Pineapples are hand delivered 
+
+Mango:
+
+Cocunut:
+
+Banana:
+
+Kiwi:
+
+Peach:
+
+Orange:
+
+Grape:
+
+Raspberry:
+				
+			</textarea>
+			
+			<!-- Topping description -->
+			<div id="itemDescriptionSpace"></div>
+			<p align = "center">
+				Topping Descriptions
+			</p>
+			<textarea cols="50" rows="18" readonly style = "resize:none">
+				This is where the user can enter text. If there's too much text, the textbox can develop scrollbars to accommodate the extra text...
+				dkfjdkfjdksajfdlkajkjfkdasjdkjfdkajfdkajkdjfdkafjdkfjdkaldj
+			</textarea>
+			
+			<!-- Order Options -->
+			<div id="itemDescriptionSpace"></div>
+			<p align = "center">
+				Order Option
+			</p>
+			<textarea cols="50" rows="4" readonly style = "resize:none">
+				This is where the user can enter text. If there's too much text, the textbox can develop scrollbars to accommodate the extra text...
+				dkfjdkfjdksajfdlkajkjfkdasjdkjfdkajfdkajkdjfdkafjdkfjdkaldj
+			</textarea>
+
 		</div>
 		<div class = "right">
-		
+			<!-- Picture for cup sizes -->
+			<div id="pictureDescriptionSpace"></div>
+			<img src="images/CustomPictures/cupSizes.jpg" alt="Login" style="width:360px;height:220px;">
+			
+			<!-- Picture for flavors -->
+			<div id="pictureDescriptionSpace"></div>
+			<img src="images/CustomPictures/smoothiePic2.jpg" alt="Login" style="width:360px;height:220px;">
+			<div id="pictureDescriptionSpace"></div>
+			<img src="images/CustomPictures/smoothiePic4.jpg" alt="Login" style="width:360px;height:220px;">
+			
+			<!-- Picture for toppings -->
+			<div id="pictureDescriptionSpace2"></div>
+			<img src="images/CustomPictures/toppingPic1.jpg" alt="Login" style="width:360px;height:220px;">
+			<div id="pictureDescriptionSpace"></div>
+			<img src="images/CustomPictures/toppingPic3.jpg" alt="Login" style="width:360px;height:220px;">
+			
+			
 		</div>
 	</div>
 	<!-- Working on creating a section for the left side -->
@@ -38,7 +121,7 @@
 		<!-- /p -->
 	<!-- /div -->
 	
-	<div id="tableSpace1"></div
+	<div id="tableSpace2"></div>
 	<p align = "center">
 		Select the Size
 	</p>
@@ -89,6 +172,37 @@
 			<td>Pineapple</td>
 			<td><input type="checkbox" name="flavors" value="XL"></td>
 		</tr>
+		<tr>
+			<td>Mango</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Cocunut</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Banana</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Kiwi</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Peach</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Orange</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Grape</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr><tr>
+			<td>Raspberry</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
 	</table>
 	
 	<div id="tableSpace1"></div>
@@ -107,11 +221,39 @@
 			<td><input type="checkbox" name="flavors" value="M"></td>
 		</tr>
 		<tr>
-			<td>Bananas</td>
+			<td>Slice Bananas</td>
 			<td><input type="checkbox" name="flavors" value="L"></td>
 		</tr>
 		<tr>
 			<td>Shredded Cocunut</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Hemp Seeds</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Nuts</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Oatmeal</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Granola</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Dried Mangos</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Raisins</td>
+			<td><input type="checkbox" name="flavors" value="XL"></td>
+		</tr>
+		<tr>
+			<td>Raspberries</td>
 			<td><input type="checkbox" name="flavors" value="XL"></td>
 		</tr>
 	</table>
@@ -135,7 +277,7 @@
 	
 	<div id="tableSpace1"></div>
 	
-	<li> <a href=special.php><img src="images/button/specialityOrderButton.png" alt="Special Page" title="Special Page"></a></li>
+	<li> <a href=order.php><img src="images/button/specialityOrderButton.png" alt="Special Page" title="Special Page"></a></li>
 	
 	
 
