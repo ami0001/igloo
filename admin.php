9ada7110b3b436ccc68547e7d21076cb4d84194c
ami0001 <ami0001@uah.edu>
2017-04-17 22:44:58 -0500

    changes to admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($_POST['submit']))
 {
-	require "adddb.php";
 	require "addtable.php";
 }
 
@@ -10,20 +9,37 @@ if (isset($_POST['drop']))
 	$con=mysqli_connect("localhost","root","","dbIgloo");
 	$null=0;
 	
-	$drop = 'DROP DATABASE dbIgloo';
-	if (mysqli_query($con, $drop)) {
-		echo "Database dbIgloo was sucessfully dropped.\n";
+	$drop1 = "DROP TABLE tbAccount";
+	$drop2 = "DROP TABLE tbMenu";
+	$drop3 = "DROP TABLE tbOrder";
+	
+	if (mysqli_query($con, $drop1)) {
+		echo "Table tbAccount was sucessfully dropped.\n";
 	}
 	else {
-		echo 'Error dropping database: '.mysql_error($con)."\n";
+		echo 'Error dropping table: '.mysql_error($con)."\n";
+	}
+	
+	if (mysqli_query($con, $drop2)) {
+		echo "Table tbMenu was sucessfully dropped.\n";
+	}
+	else {
+		echo 'Error dropping table: '.mysql_error($con)."\n";
+	}
+	
+	if (mysqli_query($con, $drop3)) {
+		echo "Table tbOrder was sucessfully dropped.\n";
+	}
+	else {
+		echo 'Error dropping table: '.mysql_error($con)."\n";
 	}
 }
 ?>
 <html>
 <body>
 <form action="admin.php" method="POST">
-<input type="submit" name="submit" value="Add Necessary Database and Tables"><br><br>
-<input type="submit" name="drop" value="Drop Database">
+<input type="submit" name="submit" value="Add Tables"><br><br>
+<input type="submit" name="drop" value="Drop Tables">
 </form>
 </body>
 </html> 
